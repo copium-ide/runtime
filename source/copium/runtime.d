29c5b9ece18f60edad0fec6697e8527ffbd1c2b9
halu <frogillius@hotmail.com>
2025-11-06 11:33:30 -0800

    added shared metadata section

--- source/copium/runtime.d
+++ source/copium/runtime.d
@@ -1,6 +1,11 @@
 module copium.runtime;
 
 // placeholder for later things. This is the library that all games made with copium will use.
+shared struct MetaData
+{
+    string author = "";
+    string project = "";
+}
 public string hi() {
     return "hewwo :3";
 }
