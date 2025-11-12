b8fb3c44cfad7732884f2a5502e2b22c48362ee8
halu <frogillius@hotmail.com>
2025-11-11 22:28:21 -0800

    fixed the incorrect variable assignment

--- source/copium/rendering/window.d
+++ source/copium/rendering/window.d
@@ -2,5 +2,5 @@ module copium.rendering.window;
 
 import bindbc.sdl;
 
-public shared float fps = 60;
-public shared float deltaTime = 1/fps;
+public enum float fps = 60;
+public enum float deltaTime = 1/fps;
