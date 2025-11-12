0394e0da4894e723898b1d6c003df9621452bbd6
halu <frogillius@hotmail.com>
2025-11-11 22:14:33 -0800

    fleshed out the plan a little more

--- source/copium/physics/three/dynamo.d
+++ source/copium/physics/three/dynamo.d
@@ -1,61 +1,93 @@
 module copium.physics.three.dynamo;
 /// Dynamo physics engine
-import copium.math.spatial.three : Vector3D;
+import copium.math.vectors.three;
 import copium.rendering.window : deltaTime;
+import copium.math.spatial.distance;
 
 public struct Dynamo
 {
     // Trying to go for XPBD type shii... wish me luck lmao
-    // TODO: This structure is highly inefficient for memory. it is better to make direct modifications to the state instead of duplicating it each time.
-    Dscene solve(Dscene scene, int iterations) {
-        foreach(DScene.MaterialBody mbody; scene.mbodies) {
-            foreach(Dscene.MaterialBody.NodeGroup ngroup; mbody.ngroups) {
-                foreach(DScene.NodeGroup.Node node; ngroup) {
-                    Vector3D raw = node.position + node.velocity*deltaTime;
-                    Vector3D[] constraints;
-                    foreach(DScene.MaterialBody.NodeGroup.Node.Constraint constraint; node.constraints) {
-
-                    }
-                }
-            }
-        }
-    }
 }
+
 public struct DScene
 {
     // TODO: Make a plan for voxel terrain and fluid simulation (which would include pressurization)
 
-    MaterialBody[] mbodies;
-    struct MaterialBody {
-        NodeGroup[] ngroups;
-        struct NodeGroup {
-            struct Node {
-                Vector3D position;
-                Vector3D velocity;
-                Constraint[] constraints;
-                struct Constraint {
-                    // TODO: do this
-                    // Damage will be subtracted from every stat when determining what type of deform.
-                    // If the force difference is greater than elastic, allow but apply a force pulling them back towards each other.
-                    // If the force difference is greater than plastic, set length to the updated distance. (and apply pulling force?? need more research)
-                    // If the force difference is greater than rigid, sever connection.
-                    // Damage = Damage + magnitude of force * some modifier
-                    // The applied forces will try to stabilize at length.
-                    float rigid;
-                    float plastic;
-                    float elastic;
-                    float stiffness; // Determines how hard the material pulls back
-                    float length; // Determines what it pulls to
-                    float damage;
-                    // Had to resist the urge to name this function "ISSBROKIE?" lmao
-                    bool isBroken() {
-                        // TODO: Too tired.
-                    }
-                }
-            }
-            
+}
+
+/// This is for making solid objects like metal sheets or blocks.
+struct SolidBody 
+{
+    NodeGroup[] ngroups;
+    struct NodeGroup
+    {
+        Node[] nodes;
+    }
+
+    struct Node
+    {
+        // TODO: find out how to define an array of constraints
+        // We could do per node, which would be difficult to coordinate.
+        // I'd like a general system where you can go solveConstraint(node1, node2), and if there is no link, it would return a velocity change of 0.
+        Vector3D position;
+        Vector3D velocity;
+        void addConstraint(Node otherNode)
+        {
+
+        }
+
+        Vector3D getConstraintVelocity(Constraint* constraint)
+        {
+            return Vector3D();
         }
     }
 
+    /// Joins two different nodes. This is my attempt at FEA.
+    struct Constraint
+    {
+        // TODO: do this
+        // Damage will be subtracted from every stat when determining what type of deform.
+        // If the force difference is greater than elastic, allow but apply a force pulling them back towards each other.
+        // If the force difference is greater than plastic, set length to the updated distance. (and apply pulling force?? need more research)
+        // If the force difference is greater than rigid, sever connection.
+        // Damage = Damage + magnitude of force * some modifier
+        // The applied forces will try to stabilize at length.
 
+        // Mechanical:
+        float tensileStrength; /// How much stress a constraint can endure before breaking.
+        float yieldStrength; /// How much stress a constraint can endure before having plastic deformation.
+        float youngMod; /// Young's modulus (how hard it resists pulling in the elastic range).
+
+        // Temperature: (in celcius)
+        // Freezing and melting point are separate for certain substances, so why would I limit this?
+        float freezingPoint; /// Temperature at which the material freezes.
+        float meltingPoint; /// Temperature at which the material melts.
+
+        float length; /// Determines what distance the solver tries to pull to.
+        float damage;
+        Node* node1;
+        Node* node2;
+
+        Vector3D[2] solveVelocities()
+        {
+            double currentLength = distance(node1.position, node2.position);
+            Vector3D[2] vectors = [
+                Vector3D(),
+                Vector3D()
+            ];
+            return vectors;
+        }
+        // Had to resist the urge to name this function "ISSBROKIE?" lmao
+        bool isBroken()
+        {
+            // TODO: Too tired.
+            return false;
+        }
+    }
+}
+struct LiquidBody
+{
+    // Outline: simulates at increasing resolution the closer it is to a solid body.
+    double minimumResolution = metersInCentimeter/10;
+    double maximumResolution = meter*50;
 }
