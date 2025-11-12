8d5970f7d859034de5834202beb44cf0555ce4d5
halu <frogillius@hotmail.com>
2025-11-11 22:14:11 -0800

    updated vector import location

--- tests/vecmath/source/main.d
+++ tests/vecmath/source/main.d
@@ -2,7 +2,7 @@ module main;
 
 import std.stdio;
 import std.math : isClose;
-import copium.math.spatial.three : Vector3D;
+import copium.math.vectors.three : Vector3D;
 
 // Helper function to check if two vectors are approximately equal
 bool areVectorsEqual(Vector3D v1, Vector3D v2, double tolerance = 1e-9) {
@@ -66,4 +66,9 @@ void main()
 
     auto cross_prod = v1.cross(Vector3D(1.0, 0.0, 0.0));
     writeln("Cross product of ", v1, " and (1,0,0) = ", cross_prod);
+
+    Vector3D vector = Vector3D(1,1,1);
+    writeln("Vector * scalar", vector * 2);
+    
+    writeln("scalar * vector", 2 * vector);
 }
