92829e2abef673ce89ba92ecda1b2c3574186fba
halu <halufun@proton.me>
2025-11-24 19:33:24 -0800

    added angle measures

---
+++ source/copium/math/measurement/angles.d
@@ -0,0 +1,54 @@
+module copium.math.measurement.angles;
+
+import std.traits : isFloatingPoint, ReturnType;
+import std.format : format;
+import std.math.constants : PI;
+
+/// Universal distance struct
+struct Distance(string name, double factor)
+{
+    double value;
+
+    @nogc this(double v)
+    {
+        this.value = v;
+    }
+
+    // Method to convert the local value into the common base unit value
+    @nogc double toBaseUnit() const
+    {
+        return this.value * factor;
+    }
+
+    // Method to convert a BaseUnit value back into (this) local unit
+    @nogc static auto fromBaseUnit(double value)
+    {
+        return typeof(this)(value / factor);
+    }
+
+    // Overload binary operators for Distance types. Converts both operands to BaseUnit, performs the operation, and returns the result converted back to the left-hand side's type (this).
+    @nogc auto opBinary(string op, T2)(T2 rhs)
+        if (is(T2 == Distance!U2, U2...)) // Constraint on the RHS type
+    {
+        static if (op == "+" || op == "-" || op == "*" || op == "/")
+        {
+            double rhsBaseUnit = rhs.toBaseUnit();
+            double lhsBaseUnit = this.toBaseUnit();
+            double resultInBaseUnit;
+
+            // Use a mixin to inject the actual operator at compile time
+            mixin(`resultInBaseUnit = lhsBaseUnit ` ~ op ~ ` rhsBaseUnit;`);
+
+            // Convert the resulting meter value back into the same type of (this)
+            return this.fromBaseUnit(resultInBaseUnit);
+        }
+        else
+        {
+            static assert(false, "Unsupported operator: " ~ op);
+        }
+    }
+}
+
+// Instantiate specific unit types:
+alias Degree = Distance!("deg", 1*(180/PI)); /// Degrees
+alias Radian = Distance!("rad", 1); /// Radians
