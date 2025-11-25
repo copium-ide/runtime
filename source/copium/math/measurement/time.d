515e23c274b6886b5bc37a72ae2f5f84dff078a7
halu <halufun@proton.me>
2025-11-24 19:28:25 -0800

    made @nogc compatible

--- source/copium/math/measurement/time.d
+++ source/copium/math/measurement/time.d
@@ -8,30 +8,25 @@ struct Time(string name, double factor)
 {
     double value;
 
-    this(double v)
+    @nogc this(double v)
     {
         this.value = v;
     }
 
-    string toString() const
-    {
-        return format("%.2f%s", this.value, name);
-    }
-
     // Method to convert the local value into the common base unit value
-    double toBaseUnit() const
+    @nogc double toBaseUnit() const
     {
         return this.value * factor;
     }
 
     // Method to convert a meter value back into (this) local unit
-    static auto fromBaseUnit(double value)
+    @nogc static auto fromBaseUnit(double value)
     {
         return typeof(this)(value / factor);
     }
 
     // Overload binary operators for Distance types. Converts both operands to the base unit, performs the operation, and returns the result converted back to the left-hand side's type (this).
-    auto opBinary(string op, T2)(T2 rhs)
+    @nogc auto opBinary(string op, T2)(T2 rhs)
         if (is(T2 == Distance!U2, U2...)) // Constraint on the RHS type
     {
         static if (op == "+" || op == "-" || op == "*" || op == "/")
