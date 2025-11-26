c86974aa0e4157db6ea95d6d4c9830da2a5c782a
halu <halufun@proton.me>
2025-11-26 01:12:42 -0800

    added window testing

--- tests/window/source/main.d
+++ tests/window/source/main.d
@@ -1,12 +1,28 @@
 module main;
 
 import std.stdio;
-import window = copium.rendering.window;
+import window = copium.windowing.window;
+import blank = copium.rendering.blank;
 
 void main(){
-    window.createWindow("bingus", 500, 500);
-    while (!window.shouldQuit){
-        
+    string[1] args;
+    args[0] = "buh";
+    window.Window window1 = window.Window("red", 50, 50, 500, 500, args);
+    blank.Renderer renderer1 = blank.Renderer(window1.getWindowContext(), 255, 0, 0);
+
+    window.Window window2 = window.Window("blue", 50, 50, 500, 500, args);
+    blank.Renderer renderer2 = blank.Renderer(window2.getWindowContext(), 0, 0, 255);
+
+    while (!((window1.closed)&&(window2.closed)))
+    {
+        window.pollEvents();
+        if (window1.shouldClose()) {
+            window1.close();
+        }
+        if (window2.shouldClose()) {
+            window2.close();
+        }
+        renderer1.render();
+        renderer2.render();
     }
-    window.quit();
 }
