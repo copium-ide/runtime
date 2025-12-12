ffb8b854852cb0d0be0af29993cc5899eddc4cc1
halu <halufun@proton.me>
2025-12-11 22:02:32 -0800

    properly use arguments

--- tests/shaders/source/main.d
+++ tests/shaders/source/main.d
@@ -6,21 +6,27 @@ import std.path : buildPath;
 import copium.rendering.single;
 import copium.windowing.window;
 import copium.math.interval;
+import copium.utils.array;
 import std.datetime;
 
 void main()
 {
     const string vertexShaderPath = "/home/halu/Documents/runtime/tests/shaders/shadercase/testing/bin/triangle.vert.spv";
     const string fragmentShaderPath = "/home/halu/Documents/runtime/tests/shaders/shadercase/testing/bin/triangle.frag.spv";
-    string[1] args;
-    args[0] = "buh";
-    Window window1 = Window("triangle", 50, 50, 500, 500, args);
+
+    Array!windowFlag args;
+    args.append(WINDOW_RESIZABLE);
+
+    Window window1 = Window("triangle", 50, 50, 500, 500, &args);
+
     writeln("making renderer");
     Renderer renderer1 = Renderer(window1.getWindowContext(), VSYNC_DISABLED);
+
     writeln("setting fragment");
     renderer1.setFragment(fragmentShaderPath);
     renderer1.setVertex(vertexShaderPath);
     renderer1.createPipeline();
+
     writeln("creating interval");
     FPS limiter = FPS(60);
     
